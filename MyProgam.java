109cc035020f9037350c1a8d869ca0ce593b6d8d
omkar14092006 <bilgucheomkar21@gmail.com>
2025-04-25 14:44:08 +0530

    Update MyProgam.java

--- MyProgam.java
+++ MyProgam.java
@@ -2,7 +2,7 @@ public class MyPogram
 {
   public static void main(String[]args)
   {
-    System.out.println("Hello this is my java Program");
+    System.out.println("Hello this is java Program");
     System.out.printLn("\nIn GitHub..");
   }
 ]
